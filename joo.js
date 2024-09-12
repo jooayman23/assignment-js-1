1a4aa7106bd22cde753850f652652420a7995ad6
jooayman23 <jooayman23@gmail.com>
2024-09-12 23:12:54 +0300

    first commit

---
+++ joo.js
@@ -0,0 +1,181 @@
+                   /**  Q1 **/
+
+// var num = Number(window.prompt('enter a number'))
+// console.log(num);
+
+                   /**  Q2 **/
+
+// var num = Number(window.prompt('enter a number'))
+// if(num %3==0 && num %4==0){
+// console.log('yes');
+// }else{
+//     console.log('no'); 
+// }
+                   /**  Q3 **/
+
+// var num1 = Number(window.prompt('one number'))                   
+// var num2 = Number(window.prompt('tow number')) 
+// if(num1>num2){
+//     console.log(num1);
+// }else if(num2>num1){
+//     console.log(num2);    
+// }else{
+//     console.log('this equal'); 
+// }  
+                   /**  Q4 **/
+
+// var num = Number(window.prompt('enter a number'))
+// if (num>0) {
+//     console.log('positive'); 
+// }else{
+//     console.log('negative');
+// }
+                   /**  Q5 **/
+
+// var num1 = Number(window.prompt('one number'))                   
+// var num2 = Number(window.prompt('tow number'))           
+// var num3 = Number(window.prompt('3   number'))           
+// if (num1 > num2 && num1 > num3) {
+//     console.log('max='+num1);   
+// }else if (num2 > num1 && num2 > num3) {
+//     console.log('max='+num2);   
+// }else if (num3 > num1 && num3 > num2) {
+//     console.log('max='+num3);   
+// }else {
+//     console.log('this equal');
+// }
+// if (num1 < num2 && num1 < num3) {
+//     console.log('min='+num1);
+// }else if (num2 < num1 && num2 < num3) {
+//     console.log('min='+num2);
+// }else if (num3 < num2 && num3 < num1) {
+//     console.log('min='+num3);
+// }
+                   /**  Q6 **/
+
+// var num = Number(window.prompt('enter a number'))
+// if (num % 2 ==0) {
+//     console.log('even');  
+// }else{
+//     console.log('odd');
+// }
+                   /**  Q7 **/
+
+// var char = window.prompt('enter character')
+// if (char == 'a' || char == 'e' || char == 'i' || char == 'o' || char == 'u') {
+//     console.log('vowel');
+// }else{
+//     console.log('consonant');
+// }
+                   /**  Q8 **/
+
+// var num = Number(window.prompt('enter a number'))
+// for (var i =1; i<=num; i++) {
+//     console.log(i);
+// }
+                   /**  Q9 **/
+
+// var num = Number(window.prompt('enter a number'))
+// for (var i =1; i<=12; i++){
+//     var joo = num * i
+//     console.log(joo);
+// }
+                   /**  Q10 **/
+
+//  var num = Number(window.prompt('enter a number'))
+// for (var i =1; i<=num; i++){
+//  if (i %2==0) {
+//     console.log(i);  
+//  }
+// }
+                   /**  Q11 **/
+
+// var num1 = Number(window.prompt('one number'))  
+// var num2 = Number(window.prompt('tow number')) 
+// console.log(num1**num2);
+      
+                   /**  Q12   **/
+
+var sup1 = Number(window.prompt('enter ur degree'))
+var sup2 = Number(window.prompt('enter ur degree'))
+var sup3 = Number(window.prompt('enter ur degree'))
+var sup4 = Number(window.prompt('enter ur degree'))
+var sup5 = Number(window.prompt('enter ur degree'))
+
+ var total = sup1+sup2+sup3+sup4+sup5
+ var aver = total / 5
+ var percentage = (total / 500)*100
+ console.log('total= '+ total);
+ console.log('average= '+ aver);
+ console.log('percentage= '+ percentage +'%');
+
+                   /**  Q13   **/
+
+// var month = Number(window.prompt('enter ur month'))
+// if ( month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 11 ) {
+//     console.log('this month are 31 days');  
+// }else if ( month == 4 || month == 6 || month == 9 || month == 12 ) {
+//     console.log('this month are 30 days');
+// }else if (month ==2) {
+//     console.log('this month are 28 days');
+// }
+                    /**  Q14   **/
+/**switch */
+                    /** Q1 **/
+//             let month = Number(window.prompt('enter ur month'))
+//                     switch (month) {
+//                         case 1 :
+//                         case 3:
+//                         case 5: 
+//                         case 7:
+//                         case 8:
+//                         case 10 :
+//                         case 11:
+//      console.log('this month are 31 days');  
+                            
+//                             break;
+//                             case 4:
+//                             case 6 :
+//                             case 9:
+//                             case 12:
+//      console.log('this month are 30 days');  
+//                     break;
+//                     case 2 :
+//      console.log('this month are 28 days');  
+// break;
+//                         default:
+//      console.log('this month are not found');  
+                           
+//                     }
+                    /** Q2 **/
+// if (char == 'a' || char == 'e' || char == 'i' || char == 'o' || char == 'u') {
+// let char = window.prompt('enter character')
+// switch (char) {
+//         case 'a':
+//         case 'e':
+//         case 'i':
+//         case 'o':
+//         case 'u':
+//     console.log('vowel');
+//                 break;
+//         default:
+//     console.log('consonant');
+// }
+// let num1 = window.prompt('one')
+// let num2 = window.prompt('tow')
+// let max1 = num1>n
+// switch (num1) {
+//         case num1>num2:
+//                 console.log(num1 = 'max');
+                
+//                 break;
+
+//         default:
+//                 break;
+// }
+
+
+
+
+ 
+ 
